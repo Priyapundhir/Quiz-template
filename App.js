8cec18f978862221f66b7908a17d62e0689a11ff
Priya Pundhir <137813639+Priyapundhir@users.noreply.github.com>
2023-08-18 11:44:00 +0530

    Quiz-via

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Quiz from "./Quiz-appp/Quiz";
+
+function App() {
+  return (
+    <>
+      <Quiz />
+    </>
+  );
+}
+
+export default App;
